eb11c8d193853e1cc67fc19622185408936525f6
Artem Kovera <artemkovera@gmail.com>
2023-03-21 10:30:31 +0200

    slight change to direct_path function

--- simple_net_4/net4.c
+++ simple_net_4/net4.c
@@ -2,18 +2,13 @@
 #include<stdio.h>
 
 double direct_path(const Network * net, const double x1, const double x2)
-{
+{   
+    
     double i1 = x1 * net->weight11 + x2 * net->weight13 + net->bias11;
-    //printf("i1 = %f\n", i1);
     double i2 = x1 * net->weight12 + x2 * net->weight14 + net->bias12;
-    //printf("i2 = %f\n", i2);
-
-    double i3 = (RELU(i1) * net->weight21) + (RELU(i2) * net->weight22) + net->bias21; 
-    //printf("i3 = %f\n", i3);
+    double i3 = (relu(i1) * net->weight21) + (relu(i2) * net->weight22) + net->bias21; 
     
-    //printf("Result = %f\n", RELU(i3));
-    
-    return RELU(i3); 
+    return relu(i3); 
 }
 
 double compute_loss_function (const Network * net, double training_set[][3], int number_of_samples)
@@ -29,5 +24,3 @@ double compute_loss_function (const Network * net, double training_set[][3], int
 
     return sum;
 }
-
-
